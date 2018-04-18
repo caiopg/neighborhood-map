8488a77bd3301075dffc79f4a16ea6a4a97034ff
Caio Gandra <caio.gandra@gmail.com>
2018-04-18 01:38:22 -0300

    Added sidemenu animation.

--- js/viewmodel/ViewModel.js
+++ js/viewmodel/ViewModel.js
@@ -1,11 +1,20 @@
 var ViewModel = function() {
   var self = this;
 
+  this.sidemenuOpen = ko.observable(false);
   this.mapPoints = ko.observableArray([]);
 
   initialPoints.forEach(function(mapPoint) {
     self.mapPoints.push(new MapPoint(mapPoint));
   });
+
+  this.openSidemenu = function() {
+    self.sidemenuOpen(true);
+  }
+
+  this.closeSidemenu = function() {
+    self.sidemenuOpen(false);
+  };
 };
 
 ko.applyBindings(new ViewModel());
