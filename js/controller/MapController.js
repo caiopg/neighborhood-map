ab09cb9cb9d86fe71b6ed5638c4267b8d0faf3b9
Caio Gandra <caio.gandra@gmail.com>
2018-04-18 00:02:07 -0300

    Adjusted layout to optimize for mobile.

--- js/controller/MapController.js
+++ js/controller/MapController.js
@@ -29,7 +29,7 @@ var initialPoints = [
 function initMap() {
   var map = new google.maps.Map(document.getElementById('map-container'), {
     center: {lat: -23.563163, lng: -46.6552573 },
-    zoom: 15
+    zoom: 14
   });
 
   initialPoints.forEach(function(mapPoint) {
